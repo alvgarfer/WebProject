4aa43b95814c8aac89b68e64c7cd5a22fd424990
alvgarfer <alvgarfer@gmail.com>
2017-02-21 18:49:27 +0100

    WebProject

---
+++ WebProject/src/dao/Consultas.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class Consultas
+{
+	public static final String CONSULTA_PATO_POR_ID = "SELECT * FROM Patologias p, Sintomas s WHERE p.id_patol = ? AND s.id_sint IN (SELECT id_sint FROM PatolSint WHERE id_patol = ?)";
+	public static final String CONSULTA_ID_PATOLOGIAS = "SELECT id_patol FROM Patologias";
+}
